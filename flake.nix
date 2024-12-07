d24d6838b2c7fe94e79ded4aa0c7b4a0afbbd7e0
Melody Kelly <melody@melody.blue>
2024-12-07 14:24:03 +1000

    use flakes

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        nativeBuildInputs = with pkgs; [
+          velero
+          fluxcd
+          sops
+          just
+        ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          inherit nativeBuildInputs;
+        };
+      }
+    );
+}
